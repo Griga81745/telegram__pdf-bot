5a9417e9edff9df433875fbcd3c301ad49cc646f
SarahLightBourne <SaraLightBourne@gmail.com>
2022-01-30 23:01:53 +0200

    Update start_help.py

--- src/handlers/start_help.py
+++ src/handlers/start_help.py
@@ -5,4 +5,4 @@ from aiogram.types import Message
 
 async def start_help(message: Message) -> None:
   colored_info(get_user(message), message.text)
-  await message.answer('Напишите команду /pdf чтобы начать', reply_markup=start_keyboard())
+  await message.answer('Стартовое сообщение', reply_markup=start_keyboard())
